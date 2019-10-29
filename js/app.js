51d1a6b576288661178f645a05304cc813889b6d
pataar <pietering1@gmail.com>
2019-10-29 14:43:21 +0100

    Fix bug with await

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@ $(function() {
 		alert("Started");
 		setInterval(async () => {
 			let r = await fetch("https://in-and-ru-store-api.uk-e1.cloudhub.io/DR/products/nl_nl/EUR/5355773200,5355772100,5088903300,5088904100");
-			let {productIsInStock} = await r.json().products.product[1].inventoryStatus;
+			let json = await r.json();
+			let {productIsInStock} = json.products.product[1].inventoryStatus;
 			if(productIsInStock){
 				new Notification("Shield is in stock");
 			}

126b26f4847ec98f71139f477328c28c33c35261
pataar <pietering1@gmail.com>
2019-10-29 13:40:26 +0100

    Add temporrary nvid check script

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
-
-$(function () {
-
+$(function() {
+	window.checkNvidia = () => {
+		Notification.requestPermission();
+		alert("Started");
+		setInterval(async () => {
+			let r = await fetch("https://in-and-ru-store-api.uk-e1.cloudhub.io/DR/products/nl_nl/EUR/5355773200,5355772100,5088903300,5088904100");
+			let {productIsInStock} = await r.json().products.product[1].inventoryStatus;
+			if(productIsInStock){
+				new Notification("Shield is in stock");
+			}
+		}, 10000);
+	};
 });

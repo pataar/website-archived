b0d9938cf9b400ef61d47bdccdf301aaaa9b9348
pataar <pietering1@gmail.com>
2019-10-29 14:47:57 +0100

    Force redeployment

--- js/app.js
+++ js/app.js
@@ -5,7 +5,9 @@ $(function() {
 		setInterval(async () => {
 			let r = await fetch("https://in-and-ru-store-api.uk-e1.cloudhub.io/DR/products/nl_nl/EUR/5355773200,5355772100,5088903300,5088904100");
 			let json = await r.json();
-			let {productIsInStock} = json.products.product[1].inventoryStatus;
+
+			let { productIsInStock } = json.products.product[1].inventoryStatus;
+
 			if(productIsInStock){
 				new Notification("Shield is in stock");
 			}

c66e4885604c92a6cf272d0185b8427cbf09411e
pataar <pietering1@gmail.com>
2020-04-20 11:43:10 +0200

    Restore nvid script

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(function() {
 		Notification.requestPermission();
 		alert("Started");
 		setInterval(async () => {
-			let r = await fetch("https://in-and-ru-store-api.uk-e1.cloudhub.io/DR/products/nl_nl/EUR/5355773200,5355772100,5088903300,5088904100");
+			let r = await fetch("https://in-and-ru-store-api.uk-e1.cloudhub.io/DR/products/nl_nl/EUR/5355773200,5355772100,5088903300,5365805400,5088904100");
 			let json = await r.json();
 
 			let { status } = json.products.product[1].inventoryStatus;
